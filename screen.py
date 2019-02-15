c9e4c26157d967b7fd7128c56ac883518b8f04a7
Soroban1729 <ruben.e.sanchez.a@hotmail.com>
2019-02-15 16:26:54 -0500

    Screen with correct colors

--- screen.py
+++ screen.py
@@ -3,9 +3,9 @@ from PIL import ImageGrab
 import cv2
 
 while(True):
-	printscreen_pil = ImageGrab.grab(bbox = (390, 157, 1515, 1019))
+	screen = np.array(ImageGrab.grab(bbox = (390, 157, 1515, 1019)))
 
-	cv2.imshow('window', np.array(printscreen_pil))
+	cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
 	if cv2.waitKey(25) & 0xFF == ord('q'):
 		cv2.destroyAllWindows()
 		break
