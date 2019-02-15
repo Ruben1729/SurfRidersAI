ad0f738358bc17c41d55040db74d6d8e6f8b60bd
Soroban1729 <ruben.e.sanchez.a@hotmail.com>
2019-02-15 16:46:21 -0500

    new methods

--- game.py
+++ game.py
@@ -2,10 +2,15 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 
-while(True):
+def cap_screen():
 	screen = np.array(ImageGrab.grab(bbox = (390, 157, 1515, 1019)))
+	return cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
 
-	cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+
+
+while(True):
+
+	cv2.imshow('window', cap_screen())
 	if cv2.waitKey(25) & 0xFF == ord('q'):
 		cv2.destroyAllWindows()
 		break
